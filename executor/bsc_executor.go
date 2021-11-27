bd121f0874c2575e11be74c760cab68a0ebe8b08
cyl19970726 <15258378443@163.com>
2021-11-27 19:02:30 +0800

    test go mod

--- executor/bsc_executor.go
+++ executor/bsc_executor.go
@@ -1,1 +1,23 @@
 package executor
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	contractabi "Bsc_Eth_swap/abi"
+)
+
+type BscExecutor struct {
+
+	Chain string
+
+	SwapAgentInst contractabi.E
+	SwapAgentAddr common.Address
+	SwapAgentAbi     abi.ABI
+	Client           *ethclient.Client
+
+}
+
+
+
